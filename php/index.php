2658505a21993bfaac5ba7fd2168488487779bad
Sirayu Petcharanon <sirayu.petcha@gmail.com>
2025-05-22 08:57:21 +0700

    rename

--- php/index.php
+++ php/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>ปฏิทินด้วย Day.js</title>
+  <title>MedBook</title>
   <script src="https://cdn.jsdelivr.net/npm/dayjs@1/dayjs.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
