3fce4b104b9ee7dcbb2680f6c6aa8a8158d168a2
Kigy Zx <ggezxao9689@gmail.com>
2025-05-23 09:22:52 +0700

    s

--- database/edit.sql
+++ database/edit.sql
@@ -1,4 +1,3 @@
--- เพิ่มตารางจองห้อง
 CREATE TABLE IF NOT EXISTS room_reservations (
     id INT AUTO_INCREMENT PRIMARY KEY,
     room_name VARCHAR(100) NOT NULL,
@@ -8,7 +7,6 @@ CREATE TABLE IF NOT EXISTS room_reservations (
     purpose TEXT
 );
 
--- เพิ่มข้อมูลตัวอย่าง
 INSERT INTO room_reservations (room_name, reserver_name, date_reserved, time_reserved, purpose) VALUES
 ('ห้องประชุม A', 'นายทานุฑัต ทิพย์เสภา', '2025-05-23', '09:00:00', 'ประชุมชมรมวิชาชีพ'),
 ('ห้องคอมพิวเตอร์ 2', 'นางสาวณัฐชยา ดรุณวรรณ', '2025-05-24', '13:30:00', 'จัดกิจกรรมอบรม Excel'),
