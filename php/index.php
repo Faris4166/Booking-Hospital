252ef2b4d3165b93c6d48c645831bb69b19a3a5f
Sirayu Petcharanon <sirayu.petcha@gmail.com>
2025-05-28 10:44:02 +0700

    Update index.php

--- php/index.php
+++ php/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>ปฏิทิน 6 แถวตายตัวด้วย Day.js</title>
+  <title>หน้าหลัก</title>
   <script src="https://cdn.jsdelivr.net/npm/dayjs@1/dayjs.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet" />
