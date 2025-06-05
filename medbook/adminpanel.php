4b341ea1e8ff38524779707fe4a1612dcfe79df5
Faris4166 <farisarthan@gmail.com>
2025-06-05 08:08:58 +0700

    มอส

---
+++ medbook/adminpanel.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+    <link rel="stylesheet" href="../css/font.css">
+    <link rel="icon" href="../image/logo.ico">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <nav class="bg-b">
+        
+    </nav>
+</body>
+
+</html>
