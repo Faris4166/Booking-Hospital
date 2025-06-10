c8edb21aaf5b2ce6e92ad8056f368ff33cf7a92d
Faris4166 <farisarthan@gmail.com>
2025-06-10 15:51:30 +0700

    สู้ๆ กัน

--- medbook/Charts.php
+++ medbook/Charts.php
@@ -3,12 +3,12 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
 <body class="pt-10 sm:pt-10 sm:ml-64 mt-10">
 
-<canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+<canvas id="myChart" style="width:500px; height:300px;"></canvas>
 
 <script>
-var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
-var yValues = [55, 49, 44, 24, 15];
-var barColors = ["red", "green","blue","orange","brown"];
+var xValues = ["ห้อง1", "ห้อง2", "ห้อง3", "ห้อง4", "ห้อง5", "ห้อง6", "ห้อง7", "ห้อง8"];
+var yValues = [55, 49, 44, 24, 50, 35, 50, 40]; // เพิ่มข้อมูลครบ 8 ห้อง
+var barColors = ["red", "green", "blue", "orange", "brown", "purple", "cyan", "magenta"]; // เพิ่มสีครบ 8
 
 new Chart("myChart", {
   type: "bar",
@@ -20,7 +20,7 @@ new Chart("myChart", {
     }]
   },
   options: {
-    legend: {display: false},
+    legend: { display: false },
     title: {
       display: true,
       text: "World Wine Production 2018"
